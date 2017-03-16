bfe18313efe93d31cc9fe14de8288a2ab90e8b30
tanzhiqiang <tzq139@outlook.com>
2017-03-16 18:47:39 +0800

    整合spring-boot 的 thymeleafdemo

--- src/main/java/cn/tzq/controller/HomeController.java
+++ src/main/java/cn/tzq/controller/HomeController.java
@@ -1,12 +1,10 @@
 package cn.tzq.controller;
 
 import cn.tzq.model.DeptVo;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.soap.Detail;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +25,7 @@ public class HomeController {
 //            method = RequestMethod.GET,
 //            produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8")
     @GetMapping("/getDeptInfo/page")
-    public String postForm(Integer pageNumber, Integer pageSize, Model model) {
+    public String getDeptInfo(Integer pageNumber, Integer pageSize, Model model) {
         List<DeptVo> deptVoList = new ArrayList<>();
         DeptVo deptInfo = null;
         for (int i = 0; i < 10; i++) {
